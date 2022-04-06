11b7770f4791a1b5e534e41df53d71016636c4aa
jwkimcan <89757058+jwkimcan@users.noreply.github.com>
2022-04-06 18:03:35 +0900

    Add files via upload

---
+++ Study/PrintF.java
@@ -0,0 +1,15 @@
+package test;
+
+public class PrintF {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String name = "David";
+		String job = "Backend";
+		int year = 2;
+		Double high = 183.3;
+		System.out.printf("안녕하세요 제이름은 %s 입니다 직업은 %s 개발자이고 공부기간은 %d 키는%.2fcm"
+				+ "입니다",name,job,year,high);
+	}
+
+}
